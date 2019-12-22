12622e51c67b512f2adf3db5bba60cac338c542c
zestdoc <zestdoc@gmail.com>
2019-12-22 12:59:56 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Wma3ZbiuQDl7cAH6EKmpN3hlC3n2kuZ2'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['tiger', 'cheetah', 'lion']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -58,7 +58,7 @@ async def homepage(request):
 @app.route('/image', methods=['POST'])
 async def analyze(request):
     img_data = await request.form()
-    img_bytes = await (img_data['file'].read())
+    img_bytes = await (img_data['files'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
